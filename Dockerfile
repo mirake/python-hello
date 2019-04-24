dce026d40ac297e2fc74b88168b72cc1c2985f1a
Rui Cao <ruicao@alauda.io>
2019-04-24 18:47:38 +0800

    Update to a new hello-world
    Signed-off-by: Rui Cao <ruicao@alauda.io>

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,15 @@
-FROM ubuntu
-MAINTAINER mirake mirake@docker.com
+FROM ubuntu:trusty
+MAINTAINER ruicao ruicao@mathildetech.com
 
 RUN apt-get update && \
-    apt-get install -y vim curl wget && \
-    apt-get install -y python-setuptools python-dev libmysqld-dev libmysqlclient-dev && \ 
-    wget http://tcpdiag.dl.sourceforge.net/project/mysql-python/mysql-python-test/1.2.4b4/MySQL-python-1.2.4b4.tar.gz && \
-    tar -zxvf MySQL-python-1.2.4b4.tar.gz && \
-    rm -f MySQL-python-1.2.4b4.tar.gz && \
-    cd MySQL-python-1.2.4b4 && \
-    ln -s /usr/bin/mysql_config /usr/local/bin/mysql_config && \
-    python setup.py build && \
-    python setup.py install && \
-    cd / && \
-    rm -f -r MySQL-python-1.2.4b4
-RUN mkdir -p /app
-ADD server.py  /app/server.py
-ADD run.sh  /run.sh
-RUN chmod 755 /run.sh
+    apt-get install -y python-pip
 
-
-# Define working directory.
 WORKDIR /app
 
-# Expose ports.
 EXPOSE 80
 
-# Define default command.
-CMD ["/run.sh"]
+COPY . /app
 
+RUN chmod +x /app/run.sh
+
+CMD ["/app/run.sh"]
--- server.py
+++ server.py
@@ -1,96 +1,34 @@
 import BaseHTTPServer
-import urlparse
-import time
-import MySQLdb
-import random
-import argparse
 import os
 
-db_host = ""
-db_port = 0
-db_user = ""
-db_pwd  = ""
-db_database = ""
 
 class WebRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        """
-        """
-        parsed_path = urlparse.urlparse(self.path)
-        message_parts = [
-                'CLIENT VALUES:',
-                'client_address=%s (%s)' % (self.client_address,
-                                            self.address_string()),
-                'command=%s' % self.command,
-                'path=%s' % self.path,
-                'real path=%s' % parsed_path.path,
-                'query=%s' % parsed_path.query,
-                'request_version=%s' % self.request_version,
-                '',
-                'SERVER VALUES:',
-                'server_version=%s' % self.server_version,
-                'sys_version=%s' % self.sys_version,
-                'protocol_version=%s' % self.protocol_version,
-                '',
-                'HEADERS RECEIVED:',
-                ]
-        for name, value in sorted(self.headers.items()):
-            message_parts.append('%s=%s' % (name, value.rstrip()))
-        message_parts.append('')
-        message = '\r\n'.join(message_parts)
-        message = showmysql(random.randint(1,3)) + "\r\n\r\n"
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(message)
+        if self.path == "/":
+            message_parts = [
+                "<html>",
+                "<head><title>Hello World</title></head>",
+                "<body style=\"text-align:center;\">",
+                "<p><img src='alauda.jpg' /></p>",
+                "<h3>Hello World! <br/><br/>Hello Alauda!</h3>",
+                "</body>",
+                "<html>"
+            ]
+            message = '\r\n'.join(message_parts)
 
-def showmysql(uid):
-    ret = ""
-    try:
-        conn=MySQLdb.connect(host=db_host, user=db_user, passwd=db_pwd, db=db_database, port=db_port)
-        cur=conn.cursor()
-        cur.execute('select * from tmp where id=%d ' % uid)
-
-        # results=cur.fetchmany(5)
-        results = cur.fetchone()
-        ret = results[1]
-
-        cur.close()
-        conn.close()
-    except MySQLdb.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        ret = "exception"
-
-    return ret
-
-
+            # message = "New request arrived from %s:%d" % self.client_address
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(message)
+        else:
+            f = open(os.curdir + os.sep + self.path)
+            self.send_response(200)
+            self.send_header('Content-type', 'image/jpg')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("-host", "--mysqlip", help='mysql ip')
-    # parser.add_argument("-port", "--mysqlport", help='mysql port')
-    # parser.add_argument("-u", "--mysqluser", help='mysql username')
-    # parser.add_argument("-p", "--mysqlpwd", help='mysql user password')
-    # parser.add_argument("-db", "--mysqldb", help='mysql database')
-    # args = parser.parse_args()
-    # print args
-
-    # db_host = args.mysqlip or "172.31.20.234"
-    # db_port = int(args.mysqlport) or 3306
-    # db_user = args.mysqluser or "root"
-    # db_pwd  = args.mysqlpwd or "12345678"
-    # db_database = args.mysqldb or "test"
-
-    db_host = os.getenv("DBHOST", "172.31.20.234")
-    db_port = int(os.getenv("DBPORT", "3306"))
-    db_user = os.getenv("DBUSER", "root")
-    db_pwd = os.getenv("DBPWD", "12345678")
-    db_database = os.getenv("DBNAME", "test")
-
-    # print showmysql(random.randint(1,3))
-    time.sleep(10)
-    print "Starting server..."
-    server = BaseHTTPServer.HTTPServer(('0.0.0.0',80), WebRequestHandler)
+    print "Server started, Listening on port 80"
+    server = BaseHTTPServer.HTTPServer(('0.0.0.0', 80), WebRequestHandler)
     server.serve_forever()
-    print "Server starte"
-
-
